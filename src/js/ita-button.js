546dcbc43e7319e35fbbc4ddfa00a5ac1b032020
MDreW <andrea.ranaldi@gmail.com>
2023-04-05 13:29:10 +0200

    fixed href function

--- src/js/ita-button.js
+++ src/js/ita-button.js
@@ -1,7 +1,7 @@
 function SpidIDP(opt) { 
   var default_values = {
   addition: '<a href="https://www.spid.gov.it">Maggiori informazioni</a><a href="https://www.spid.gov.it/richiedi-spid">Non hai SPID?</a><a href="https://www.spid.gov.it/serve-aiuto">Serve aiuto?</a>',
-  href: function (data,params) {`${params.get("return")}?entityID=${data["entity_id"]}&return=${params.get("return")}`},
+  href: function (data,params) {return `${params.get("return")}?entityID=${data["entity_id"]}&return=${params.get("return")}`},
   url: "https://registry.spid.gov.it/entities-idp?&output=json&custom=info_display_base",
   idps: [{"organization_name": "AArubaPEC S.p.A.", "entity_id": "https://loginspid.aruba.it", "logo_uri": "img/spid-idp-arubaid.svg"},{"organization_name": "InfoCert S.p.A.", "entity_id": "https://identity.infocert.it", "logo_uri": "img/spid-idp-infocertid.svg"},{"organization_name": "IN.TE.S.A. S.p.A.", "entity_id": "https://spid.intesa.it", "logo_uri": "img/spid-idp-intesaid.svg"},{"organization_name": "Lepida S.p.A.", "entity_id": "https://id.lepida.it/idp/shibboleth", "logo_uri": "img/spid-idp-lepidaid.svg"},{"organization_name": "Namirial", "entity_id": "https://idp.namirialtsp.com/idp", "logo_uri": "img/spid-idp-namirialid.svg"},{"organization_name": "Poste Italiane SpA", "entity_id": "https://posteid.poste.it", "logo_uri": "img/spid-idp-posteid.svg"},{"organization_name": "Sielte S.p.A.", "entity_id": "https://identity.sieltecloud.it", "logo_uri": "img/spid-idp-sielteid.svg"},{"organization_name": "Register.it S.p.A.", "entity_id": "https://spid.register.it", "logo_uri": "img/spid-idp-spiditalia.svg"},{"organization_name": "TI Trust Technologies srl", "entity_id": "https://login.id.tim.it/affwebservices/public/saml2sso", "logo_uri": "img/spid-idp-timid.svg"},{"organization_name": "TeamSystem s.p.a.", "entity_id": "https://spid.teamsystem.com/idp", "logo_uri": "img/spid-idp-teamsystemid.svg"}]
 };
@@ -9,7 +9,7 @@ function SpidIDP(opt) {
   
   var add_idp_entry = function (data, element, params) {
     let a = document.createElement('a');
-    a.setAttribute('href', this.href(data, params));
+    a.setAttribute('href', href(data, params));
     a.innerHTML = `<img src="${data["logo_uri"]}" alt="${data["organization_name"]}">`
     element.prepend(a)
   }
